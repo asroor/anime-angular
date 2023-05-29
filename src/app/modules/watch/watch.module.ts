cb45f159848c360780bcf76b751a968e6799861a
asroor <asrorgithub@gmail.com>
2023-05-29 23:31:42 +0500

    video

--- src/app/modules/watch/watch.module.ts
+++ src/app/modules/watch/watch.module.ts
@@ -4,6 +4,7 @@ import { WatchPageComponent } from './components/watch-page/watch-page.component
 import { VideoComponent } from './components/video/video.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MovieAboutComponent } from './components/movie-about/movie-about.component';
+import { SharedModule } from "../../shared/shared.module";
 
 const routes: Routes = [
   { path: '', component: WatchPageComponent }
@@ -18,6 +19,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    SharedModule
   ]
 })
 export class WatchModule { }
